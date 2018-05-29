3861700a81298de05b1a0db11dc8797af82a9b7d
mkczarkowski <marcinczarkowski@onet.eu>
2018-05-29 19:52:48 +0200

    refactor: move table's header and toolbar components to separate files

---
+++ app/modules/home/components/DeviceTable/EnhancedTable/EnhancedTableHead.js
@@ -0,0 +1,94 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TableHead from "@material-ui/core/es/TableHead/TableHead";
+import TableRow from "@material-ui/core/es/TableRow/TableRow";
+import TableCell from "@material-ui/core/es/TableCell/TableCell";
+import Checkbox from "@material-ui/core/es/Checkbox/Checkbox";
+import Tooltip from "@material-ui/core/es/Tooltip/Tooltip";
+import TableSortLabel from "@material-ui/core/es/TableSortLabel/TableSortLabel";
+
+const columnData = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Nazwa urządzenia"
+  },
+  {
+    id: "powerConsumption",
+    numeric: false,
+    disablePadding: true,
+    label: "Zużycie energii [w]"
+  },
+  {
+    id: "timeUsed",
+    numeric: false,
+    disablePadding: true,
+    label: "Czas użycia [h/mth]"
+  }
+];
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property);
+  };
+
+  render() {
+    const {
+      onSelectAllClick,
+      order,
+      orderBy,
+      numSelected,
+      rowCount
+    } = this.props;
+
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={numSelected === rowCount}
+              onChange={onSelectAllClick}
+            />
+          </TableCell>
+          {columnData.map(column => {
+            return (
+              <TableCell
+                key={column.id}
+                numeric={column.numeric}
+                padding={column.disablePadding ? "none" : "default"}
+                sortDirection={orderBy === column.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={column.numeric ? "bottom-end" : "bottom-start"}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === column.id}
+                    direction={order}
+                    onClick={this.createSortHandler(column.id)}
+                  >
+                    {column.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            );
+          }, this)}
+        </TableRow>
+      </TableHead>
+    );
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired
+};
+
+export default EnhancedTableHead;
