1db0e1aa6f52034d7f8a2583a5296043fd270998
mkczarkowski <marcinczarkowski@onet.eu>
2018-05-31 13:51:42 +0200

    chore: set fixed max height of app window

--- app/main.development.js
+++ app/main.development.js
@@ -1,69 +1,66 @@
 // @flow
-import { app, BrowserWindow } from 'electron'
-import MenuBuilder from './menu'
+import { app, BrowserWindow } from "electron";
+import MenuBuilder from "./menu";
 
-let mainWindow = null
+let mainWindow = null;
 
-if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support')
-  sourceMapSupport.install()
+if (process.env.NODE_ENV === "production") {
+  const sourceMapSupport = require("source-map-support");
+  sourceMapSupport.install();
 }
 
-if (process.env.NODE_ENV === 'development') {
-  require('electron-debug')()
-  const path = require('path')
-  const p = path.join(__dirname, '..', 'app', 'node_modules')
-  require('module').globalPaths.push(p)
+if (process.env.NODE_ENV === "development") {
+  require("electron-debug")();
+  const path = require("path");
+  const p = path.join(__dirname, "..", "app", "node_modules");
+  require("module").globalPaths.push(p);
 }
 
 const installExtensions = async () => {
-  const installer = require('electron-devtools-installer')
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS
-  const extensions = [
-    'REACT_DEVELOPER_TOOLS',
-    'REDUX_DEVTOOLS'
-  ]
+  const installer = require("electron-devtools-installer");
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = ["REACT_DEVELOPER_TOOLS", "REDUX_DEVTOOLS"];
 
-  return Promise
-    .all(extensions.map(name => installer.default(installer[name], forceDownload)))
-    .catch(console.log)
-}
+  return Promise.all(
+    extensions.map(name => installer.default(installer[name], forceDownload))
+  ).catch(console.log);
+};
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('ready', async () => {
-  if (process.env.NODE_ENV === 'development') {
-    await installExtensions()
+app.on("ready", async () => {
+  if (process.env.NODE_ENV === "development") {
+    await installExtensions();
   }
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728
-  })
+    width: 1366,
+    maxHeight: 742
+  });
 
-  mainWindow.loadURL(`file://${__dirname}/app.html`)
+  mainWindow.loadURL(`file://${__dirname}/app.html`);
 
   // @TODO: Use 'ready-to-show' event
   //        https://github.com/electron/electron/blob/master/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
+  mainWindow.webContents.on("did-finish-load", () => {
     if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined')
+      throw new Error('"mainWindow" is not defined');
     }
-    mainWindow.show()
-    mainWindow.focus()
-  })
+    mainWindow.show();
+    mainWindow.focus();
+  });
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 
-  const menuBuilder = new MenuBuilder(mainWindow)
-  menuBuilder.buildMenu()
-})
+  const menuBuilder = new MenuBuilder(mainWindow);
+  menuBuilder.buildMenu();
+});
