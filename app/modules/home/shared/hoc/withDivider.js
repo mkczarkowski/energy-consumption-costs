80420c19cabd91c115ef951c822764055a825f85
mkczarkowski <marcinczarkowski@onet.eu>
2018-06-02 11:09:07 +0200

    fix: remove unnecessary divider margins

--- app/modules/home/shared/hoc/withDivider.js
+++ app/modules/home/shared/hoc/withDivider.js
@@ -1,17 +1,12 @@
 import React, { Fragment } from "react";
 import Divider from "@material-ui/core/es/Divider/Divider";
 
-const styles = {
-  divider: {
-    // marginTop: 16
-  }
-};
 
 export default Component => {
   return props => (
     <Fragment>
       <Component {...props} />
-      <Divider style={styles.divider} />
+      <Divider />
     </Fragment>
   );
 };
